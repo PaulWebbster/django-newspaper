da66b668c7fe35b84bfdc0c7501173377593f21b
Pawel Weber <pawelweber@yahoo.pl>
2016-02-12 06:53:39 +0100

    Test module with django-cms and ckeditor, Change News model and add filtering and list display to NewAdmin.

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from models import News
 # Register your models here.
+from django.utils.translation import ugettext_lazy as _
+
+
+class AdminNews(admin.ModelAdmin):
+    list_display = ('title', 'author', 'get_is_visible', 'pub_date')
+    list_filter = ('author', 'pub_date', 'status')
+
+    def get_is_visible(self, entry):
+        return entry.is_visible
+
+    get_is_visible.boolean = True
+    get_is_visible.short_description = _('Widoczny')
+
+admin.site.register(News, AdminNews)
--- newspaper/models.py
+++ newspaper/models.py
@@ -1,19 +1,43 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
-from djangocms_text_ckeditor.fields import HTMLField
 from django.contrib.auth.models import User
-# Create your models here.
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserFullName(User):
+    class Meta:
+        proxy = True
+
+    def __unicode__(self):
+        return self.get_full_name()
 
 
 class News(models.Model):
+    SZKIC = 0
+    OPUBLIKOWANY = 1
+
     STATUS_CHOICES = (
-        (0, 'szkic'),
-        (1, 'opublikowany')
+        (SZKIC, _('Szkic')),
+        (OPUBLIKOWANY, _('Opublikowany'))
     )
 
-    status = models.IntegerField("Status", max_length=1, choices=STATUS_CHOICES)
-    lead = HTMLField("Akapit wiodacy")
-    news_text = HTMLField("Pelen tekst")
-    pub_date = models.DateTimeField("Data publikacji")
-    autor = models.OneToOneField(User)
+    status = models.IntegerField(_("Status"), max_length=1, choices=STATUS_CHOICES)
+    title = models.CharField(_("Tytuł"), max_length=255)
+    lead = RichTextUploadingField(_("Akapit wiodący"))
+    article = RichTextUploadingField(_("Tekst główny"), blank=True)
+    pub_date = models.DateTimeField(_("Data publikacji"))
+    author = models.ForeignKey(UserFullName, verbose_name=_("Autor"))
+
+    @property
+    def is_visible(self):
+        return True if self.status == self.OPUBLIKOWANY else False
+
+#    visible = property(visible_property)
+
+    class Meta:
+        verbose_name = _("Wiadomość")
+        verbose_name_plural = _("Wiadomości")
