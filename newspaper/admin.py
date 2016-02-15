96d009b5915216592cdb3f458a2a9687b1bfe499
Pawel Weber <pawelweber@yahoo.pl>
2016-02-15 23:36:56 +0100

    Add get_changeform_initial_data into admin.py to model form startup fill-up.

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
-from models import News
-# Register your models here.
+
+from newspaper.models.news import News
 from django.utils.translation import ugettext_lazy as _
 
 
-class AdminNews(admin.ModelAdmin):
+class NewsAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'get_is_visible', 'pub_date')
     list_filter = ('author', 'pub_date', 'status')
 
+    @staticmethod
     def get_is_visible(self, entry):
         return entry.is_visible
+
+    def get_changeform_initial_data(self, request):
+        """
+        Provide initial datas when creating an entry
+        """
+        get_data = super(NewsAdmin, self).get_changeform_initial_data(request)
+        get_data['author'] = request.user.pk
+        return get_data
 
     get_is_visible.boolean = True
     get_is_visible.short_description = _('Widoczny')
 
-admin.site.register(News, AdminNews)
+admin.site.register(News, NewsAdmin)
