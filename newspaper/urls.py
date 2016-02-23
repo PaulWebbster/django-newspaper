eddb218c094aad9af15bd57c7518c094ff977ddd
Pawel Weber <pawelweber@yahoo.pl>
2016-02-24 00:22:12 +0100

    Add archival news display

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -2,10 +2,15 @@ from django.conf.urls import url
 
 from views.news import NewsBaseView
 from views.news import NewsListView
+from views.news import NewsArchivalListView
 from views.author import AuthorDetail
 
 urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', NewsBaseView.as_view(), name='news_detail'),
     url(r'^list/$', NewsListView.as_view(), name='news_list'),
+    url(r'^list/(?P<year>[0-9]+)/$', NewsArchivalListView.as_view(), name='news_list_archival'),
+    url(r'^list/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$', NewsArchivalListView.as_view(), name='news_list_archival'),
+    url(r'^list/(?P<year>[0-9]+)/(?P<month>[0-9]+)/(?P<day>[0-9]+)/$', NewsArchivalListView.as_view(),
+        name='news_list_archival'),
     url(r'^author/(?P<pk>\d+)/$', AuthorDetail.as_view(), name='author_detail'),
 ]
--- newspaper/views/news.py
+++ newspaper/views/news.py
@@ -4,6 +4,9 @@ from django.views.generic import View
 from ..models.news import News
 from ..models.news import Author
 from django.contrib.auth.models import User
+import datetime
+
+NEWS_PER_PAGE = 3
 
 class NewsBaseView(View):
     """
@@ -20,9 +23,40 @@ class NewsBaseView(View):
 
 class NewsListView(NewsBaseView, ListView):
     model = News
-    paginate_by = 5
+    paginate_by = NEWS_PER_PAGE
 
     def get_context_data(self, **kwargs):
         context = super(NewsListView, self).get_context_data(**kwargs)
+
+        return context
+
+class NewsArchivalListView(ListView):
+    #template_name = 'books/books_by_publisher.html'
+    model = News
+    paginate_by = NEWS_PER_PAGE
+    template_name = "django_newspaper/arch_news_list.html"
+
+    def get_queryset(self):
+        if 'day' in self.kwargs:
+            requested_date = datetime.date(int(self.kwargs['year']), int(self.kwargs['month']), int(self.kwargs['day']))
+            return News.objects.all().filter(pub_date__contains=requested_date)
+        elif 'month' in self.kwargs:
+            return News.objects.all().filter(pub_date__year=self.kwargs['year'], pub_date__month=self.kwargs['month'])
+        elif 'year' in self.kwargs:
+            return News.objects.all().filter(pub_date__year=self.kwargs['year'])
+
+    def get_context_data(self, **kwargs):
+        context = super(NewsArchivalListView, self).get_context_data(**kwargs)
+
+        link = ""
+
+        if 'year' in self.kwargs:
+            link += "%s/" % self.kwargs['year']
+        if 'month' in self.kwargs:
+            link += "%s/" % self.kwargs['month']
+        if 'day' in self.kwargs:
+            link += "%s/" % self.kwargs['day']
+
+        context['link'] = link
 
         return context
